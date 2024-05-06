22c7c46b2a36382374ab76a1205b62408b3a74f9
maryamnawas <maryamnawas2002@gmail.com>
2024-05-06 10:39:55 +0530

    done with appointment

--- src/main/java/csa/healthsystem/dao/AppointmentDAO.java
+++ src/main/java/csa/healthsystem/dao/AppointmentDAO.java
@@ -8,3 +8,105 @@ package csa.healthsystem.dao;
  *
  * @author Maryam
  */
+import csa.healthsystem.model.Appointment;
+import csa.healthsystem.exception.DatabaseException;
+import csa.healthsystem.exception.NotFoundException;
+import csa.healthsystem.model.Patient;
+import csa.healthsystem.model.Doctor;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppointmentDAO {
+    private static final Logger LOGGER = Logger.getLogger(AppointmentDAO.class.getName());
+
+    private static final List<Appointment> appointments = new ArrayList<>();
+    private static final AtomicInteger nextId = new AtomicInteger(1);
+    private final PatientDAO patientDAO = new PatientDAO();
+    private final DoctorDAO doctorDAO = new DoctorDAO();
+
+    public AppointmentDAO() throws ParseException {
+        // Adding sample appointments
+        addSampleAppointments();
+    }
+
+    private void addSampleAppointments() {
+        try {
+            // Fetch patients and doctors from DAO
+            Patient patient1 = patientDAO.getPatientById(1000);
+            Patient patient2 = patientDAO.getPatientById(1001);
+            Patient patient3 = patientDAO.getPatientById(1002);
+            Doctor doctor1 = doctorDAO.getDoctorById(1);
+            Doctor doctor2 = doctorDAO.getDoctorById(2);
+            Doctor doctor3 = doctorDAO.getDoctorById(3);
+
+            // Define appointment data
+            String[] appointmentDates = {"2024-05-10", "2024-05-12", "2024-05-15"};
+            String[] appointmentTimes = {"10:00 AM", "11:30 AM", "02:00 PM"};
+
+            // Create appointments for each combination of patient and doctor
+            for (int i = 0; i < 3; i++) {
+                String appointmentDate = appointmentDates[i];
+                String appointmentTime = appointmentTimes[i];
+
+                Appointment appointment1 = new Appointment(nextId.getAndIncrement(), patient1, doctor1, appointmentDate, appointmentTime, "Diagnostic Test");
+                Appointment appointment2 = new Appointment(nextId.getAndIncrement(), patient2, doctor2, appointmentDate, appointmentTime, "Regular Checkup");
+                Appointment appointment3 = new Appointment(nextId.getAndIncrement(), patient3, doctor3, appointmentDate, appointmentTime, "Follow-up Consultation");
+
+                appointments.add(appointment1);
+                appointments.add(appointment2);
+                appointments.add(appointment3);
+            }
+        } catch (NotFoundException e) {
+            LOGGER.log(Level.SEVERE, "Error adding sample appointments: " + e.getMessage());
+        }
+    }
+
+
+    public List<Appointment> getAllAppointments() {
+        LOGGER.log(Level.INFO, "Retrieving all appointments");
+        return new ArrayList<>(appointments); // Return a copy of the list to prevent modification
+    }
+
+    public Appointment getAppointmentById(int id) {
+        LOGGER.log(Level.INFO, "Retrieving appointment by ID: " + id);
+        for (Appointment appointment : appointments) {
+            if (appointment.getId() == id) {
+                LOGGER.log(Level.INFO, "Appointment with ID " + id + " found: " + appointment);
+                return appointment;
+            }
+        }
+        LOGGER.log(Level.WARNING, "Appointment with ID " + id + " not found");
+        throw new NotFoundException("Appointment with ID " + id + " not found");
+    }
+
+    public void addAppointment(Appointment appointment) {
+        LOGGER.log(Level.INFO, "Adding new appointment: " + appointment);
+        appointment.setId(nextId.getAndIncrement());
+        appointments.add(appointment);
+    }
+
+    public void updateAppointment(int id, Appointment updatedAppointment) {
+        LOGGER.log(Level.INFO, "Updating appointment with ID: " + id);
+        Appointment existingAppointment = getAppointmentById(id);
+        existingAppointment.setPatient(updatedAppointment.getPatient());
+        existingAppointment.setDoctor(updatedAppointment.getDoctor());
+        existingAppointment.setAppointmentDate(updatedAppointment.getAppointmentDate());
+        existingAppointment.setAppointmentTime(updatedAppointment.getAppointmentTime());
+        existingAppointment.setPurpose(updatedAppointment.getPurpose());
+        LOGGER.log(Level.INFO, "Appointment updated: " + existingAppointment);
+    }
+
+    public void deleteAppointment(int id) {
+        LOGGER.log(Level.INFO, "Deleting appointment with ID: " + id);
+        Appointment appointmentToDelete = getAppointmentById(id);
+        appointments.remove(appointmentToDelete);
+        LOGGER.log(Level.INFO, "Appointment deleted: " + appointmentToDelete);
+    }
+}
--- src/main/java/csa/healthsystem/model/Appointment.java
+++ src/main/java/csa/healthsystem/model/Appointment.java
@@ -8,130 +8,81 @@ package csa.healthsystem.model;
  *
  * @author Maryam
  */
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- * Appointment class represents an appointment scheduled between a patient and a doctor.
- */
 public class Appointment {
-    private static final Logger LOGGER = Logger.getLogger(Appointment.class.getName());
-
-    private int id; // New ID attribute
-    private Date date;
-    private String time;
+    private int id;
     private Patient patient;
     private Doctor doctor;
-    
-    // Default constructor
-    public Appointment() {}
-    
-    /**
-     * Constructor to initialize an Appointment object with the provided attributes.
-     * @param id The ID of the appointment.
-     * @param date The date of the appointment.
-     * @param time The time of the appointment.
-     * @param patient The patient scheduled for the appointment.
-     * @param doctor The doctor scheduled for the appointment.
-     */
-    public Appointment(int id, Date date, String time, Patient patient, Doctor doctor) {
+    private String appointmentDate;
+    private String appointmentTime;
+    private String purpose;
+
+    public Appointment(int id, Patient patient, Doctor doctor, String appointmentDate, String appointmentTime, String purpose) {
         this.id = id;
-        this.date = date;
-        this.time = time;
         this.patient = patient;
         this.doctor = doctor;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.purpose = purpose;
     }
 
     // Getters and setters
-    
-    /**
-     * Get the id of the appointment.
-     * @return The id of the appointment.
-     */
     public int getId() {
-        LOGGER.log(Level.INFO, "Getting ID: " + id);
         return id;
     }
 
-    /**
-     * Set the id of the appointment.
-     * @param id The date of the appointment to set.
-     */
     public void setId(int id) {
-        LOGGER.log(Level.INFO, "Setting ID: " + id);
         this.id = id;
     }
 
-    /**
-     * Get the date of the appointment.
-     * @return The date of the appointment.
-     */
-    public Date getDate() {
-        LOGGER.log(Level.INFO, "Getting date: " + date);
-        return date;
-    }
-
-    /**
-     * Set the date of the appointment.
-     * @param date The date of the appointment to set.
-     */
-    public void setDate(Date date) {
-        LOGGER.log(Level.INFO, "Setting date: " + date);
-        this.date = date;
-    }
-
-    /**
-     * Get the time of the appointment.
-     * @return The time of the appointment.
-     */
-    public String getTime() {
-        LOGGER.log(Level.INFO, "Getting time: " + time);
-        return time;
-    }
-
-    /**
-     * Set the time of the appointment.
-     * @param time The time of the appointment to set.
-     */
-    public void setTime(String time) {
-        LOGGER.log(Level.INFO, "Setting time: " + time);
-        this.time = time;
-    }
-
-    /**
-     * Get the patient scheduled for the appointment.
-     * @return The patient scheduled for the appointment.
-     */
     public Patient getPatient() {
-        LOGGER.log(Level.INFO, "Getting patient: " + patient);
         return patient;
     }
 
-    /**
-     * Set the patient scheduled for the appointment.
-     * @param patient The patient scheduled for the appointment to set.
-     */
     public void setPatient(Patient patient) {
-        LOGGER.log(Level.INFO, "Setting patient: " + patient);
         this.patient = patient;
     }
 
-    /**
-     * Get the doctor scheduled for the appointment.
-     * @return The doctor scheduled for the appointment.
-     */
     public Doctor getDoctor() {
-        LOGGER.log(Level.INFO, "Getting doctor: " + doctor);
         return doctor;
     }
 
-    /**
-     * Set the doctor scheduled for the appointment.
-     * @param doctor The doctor scheduled for the appointment to set.
-     */
     public void setDoctor(Doctor doctor) {
-        LOGGER.log(Level.INFO, "Setting doctor: " + doctor);
         this.doctor = doctor;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", patient=" + patient +
+                ", doctor=" + doctor +
+                ", appointmentDate='" + appointmentDate + '\'' +
+                ", appointmentTime='" + appointmentTime + '\'' +
+                ", purpose='" + purpose + '\'' +
+                '}';
     }
 }
